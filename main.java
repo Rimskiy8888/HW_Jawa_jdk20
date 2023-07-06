7da5f43b658ab86381bddb9a4192ce8110473558
Rimskiy8888 <137941314+Rimskiy8888@users.noreply.github.com>
2023-07-06 23:48:50 +0200

    HomeWork1_JAWA_Jdk20
    HomeWork1_JAWA_Jdk20
    6.07.2023

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        int number1 = 50;
+        int number2 = 19;
+        int number3 = (number1+number2);
+        int number4 = (number1-number2);
+        int number5 = (number1*number2);
+        int number6 = (number1/number2);
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+    }
+}
